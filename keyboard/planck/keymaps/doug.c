453d58cb5cff5c9803b73140bcea0b151015fee1
Doug Black <dblack@twilio.com>
2016-06-14 17:10:48 -0700

    Random updates

--- keyboard/planck/keymaps/doug.c
+++ keyboard/planck/keymaps/doug.c
@@ -38,13 +38,13 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * | Shift|   Z  |   X  |   C  |   V  |   B  |   N  |   M  |   ,  |   .  |   /  |Shift |
  * |------+------+------+------+------+------+------+------+------+------+------+------|
  * | Brite| Alt  | Tab  | GUI  |Lower |    Space    |Raise | Enter| Down |  Up  |Right |
- * `-----------------------------------------------------------------------------------'    
+ * `-----------------------------------------------------------------------------------'
  */
 [_MAIN] = {
   {KC_ESC,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSPC},
   {KC_LCTL, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT},
   {KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT},
-  {LLEFT,   KC_LALT, KC_TAB,  KC_LGUI, LOWER,   KC_SPC,  KC_SPC,  RAISE,   KC_ENT,  KC_DOWN, KC_UP,   LRGHT}
+  {LLEFT,   KC_LALT, _______, KC_LGUI, LOWER,   KC_SPC,  KC_SPC,  RAISE,   KC_ENT,  _______, KC_RALT, LRGHT}
 },
 
 /* Lower
